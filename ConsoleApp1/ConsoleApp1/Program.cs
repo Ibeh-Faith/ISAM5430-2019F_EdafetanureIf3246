669c8e990f8b39fdca97972ddd6625ec01001df7
edafetanureif3246 <edafetanureif3246@SSCB03-45611>
2019-08-29 22:22:28 -0500

    new app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System; //java's import
+/*
+  multiline comment
+ */
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAM5430.Classs01A
+{
+    //mikeywu5, borismo1992
+    class Program
+    {
+        // entry-point in the console app
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Welcome to c# Programming"); 
+            //since we already declared System up, we can remove it
+            // Console.WriteLine("Welcome to c# Programming");
+
+            //declaring a vairable as a string
+            string person = "Michael"; //initializing a vairable
+            // string interpolation
+            Console.WriteLine($"Welcome to c# programming, {person}.");
+
+            Console.WriteLine("Welcome to c# programming, {0}.", person);
+
+
+            Console.WriteLine(string.Format("Welcome to c# programming, {0}.", person));
+
+            Console.WriteLine("Welcome to c# programming," + person + ".");
+
+            /*
+             * variables:
+             * Data types:
+             * 
+             * simple types
+             *   int
+             *   bool(true/false)
+             *   char('a', '0' != 0)
+             *   double/float(IEE 754)
+             *   decimal software-implementation of double/float
+             *   byte (binary contents)
+             *   
+             * complex types
+             *   string
+             *   arrays(int[])
+             *   classes
+             *   struct
+             *   
+             * Arithmetic Operators
+             *    +, -, *, /, %
+             *    
+             * Boolean Operators
+             *    ==, !=, >, <, >=, <=, &&, ||, !
+             */
+        }
+    }
+}
